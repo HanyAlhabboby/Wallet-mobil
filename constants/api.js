85b5dd89c67087cf11f95a6bff1c0f5a72a91c9d
Bumbaclat89 <HANY89@LIVE.SE>
2025-08-11 10:13:14 +0200

    mobil

---
+++ constants/api.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://wallet-api-zgvj.onrender.com/api";
+//export const API_URL = "https://localhost:5001/api";
+//npm run dev to make the local server active
+//npx expo to make the other server active
